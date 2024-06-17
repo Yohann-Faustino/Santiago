cf38588b9cfe00c2a402a99e948f4e69ffbd712e
Yohann-Faustino <yohannfaustino@gmail.com>
2024-06-17 19:25:31 +0200

    creation de toutes les pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,58 @@
+import {RouterProvider, createBrowserRouter} from "react-router-dom";
 import './App.css'
+
+// On cree une constante monRouter dans laquel on definis les routes de chaques pages.
+const monRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Page d'Accueil</div>
+  },
+  {
+    path: '/signup',
+    element: <div>Page Connexion</div>
+  },
+  {
+    path: '/contact',
+    element: <div>Page Contact</div>
+  },
+  {
+    path: '/legalDisclaimer',
+    element: <div>Page Mentions légales</div>
+  },
+  {
+    path: '/siteMap',
+    element: <div>Page Plan du site</div>
+  },
+  {
+    path: '/prestations',
+    element: <div>Page des Prestations</div>
+  },
+  {
+    path: '/planning',
+    element: <div>Page Planning</div>
+  },
+  {
+    path: '/adminBoard',
+    element: <div>Page Tableau de bord</div>
+  },
+  {
+    path: '/404',
+    element: <div>Page Error</div>
+  },
+  {
+    path: '/profil',
+    element: <div>Page Profil</div>
+  },
+  {
+    path: '/commenter',
+    element: <div>Page des Commentaires</div>
+  }
+])
+
 
 function App() {
 
-  return (
-    <>
-
-      <h1>Santiago</h1>
-
-    </>
-  )
+  return <RouterProvider router={monRouter}/>
 }
 
-export default App
+export default App;
