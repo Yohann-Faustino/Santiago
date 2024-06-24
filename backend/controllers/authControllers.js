e93679a6c6cdb288a2ed67d54a7ed97dce9260c1
Yohann-Faustino <yohannfaustino@gmail.com>
2024-06-24 14:12:05 +0200

    deplacement du validator et bcrypt dans authcontroller

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -3,9 +3,34 @@ import validator from "validator";
 import bcrypt from "bcrypt";
 import crypto from "node:crypto";
 
+// On configure le validator:
+const validatePassword = (password) => {
+    const isPasswordValid = validator.isStrongPassword(password, {
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1, // Nombre minimum de caractères spéciaux requis.
+    });
+
+    return isPasswordValid;
+};
+
+// On configure le hash:
+const hashPassword = async (password) => {
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10); // Dix est le nombre optimal de tours de hash pour obtenir un password super secret.
+        return hashedPassword;
+    } catch (error) {
+        throw new Error('Erreur lors du hashage du mot de passe', error)
+    }
+};
+
+
+
 const authController = {
 
-// Fontion pour gérer l'inscription
+// Fontion pour gérer l'inscription:
 signup: async (req, res) => {
     const { firstname, lastname, address, phone, email, password } = req.body;
 
@@ -16,10 +41,15 @@ signup: async (req, res) => {
             return res.status(400).json({message: 'Cet email est déjà utilisé.'});
         }
 
-        // On hash le mot de passe
-        const hashedPassword = await bcrypt.hash(password, 10); // Dix est le nombre optimal de tours de hash pour obtenir un password super secret
+        // On vérifie si le password respecte nos conditions:
+        if(!validatePassword(password)) {
+            return res.status(400).json({message: 'Le mot de passe doit contenir 8 caractères minimums, une majuscule, une minuscule, un chiffre et un caractère spécial.'})
+        }
 
-        // On crée un customers
+        // On hash le mot de passe:
+        const hashedPassword = await hashPassword(password);
+
+        // On crée un customers:
         const newCustomer = await Customers.create({
             firstname,
             lastname,
@@ -35,34 +65,5 @@ signup: async (req, res) => {
         res.status(500).json({message:'Dommage, votre inscription n\'a pas abboutie !'});
     }
 }};
-
-const validatePassword = (password) => {
-    const isPasswordValid = validator.isStrongPassword(password, {
-        minLength: 8,
-        minLowercase: 1,
-        minUppercase: 1,
-        minNumbers: 1,
-        minSymbols: 1, // Nombre minimum de caractères spéciaux requis
-        returnScore: false, // Retourne un score de robustesse du mot de passe si true
-        pointsPerUnique: 1,
-        pointsPerRepeat: 0.5,
-        pointsForContainingLower: 10,
-        pointsForContainingUpper: 10,
-        pointsForContainingNumber: 10,
-        pointsForContainingSymbol: 10
-    });
-
-    return isPasswordValid;
-}
-
-const saltRounds = 10; // Définis le nombre de tours de cryptage du mot de passe pour créer un hash le plus optimal (10 tours est le chiffre idéale).
-const hashPassword = async (password) => {
-    try {
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
-        return hashedPassword;
-    } catch (error) {
-        throw new Error('Erreur lors du hashage du mot de passe', error)
-    }
-};
 
 export default authController;
