c05cb4d4415e836f0c402c94324c6814157a9558
Yohann-Faustino <yohannfaustino@gmail.com>
2024-07-16 13:50:40 +0200

    affichage des users de la bdd dans le dashboard de l'admin: ok

--- backend/components/users/user.jsx
+++ backend/components/users/user.jsx
@@ -1,5 +1,5 @@
-// Permet de naviguer vers une autre page (useredit) tout en passant un paramètre d'ID utilisateur:
-import React, { useState } from "react";
+// useState crée une 'boîte' vide pour stocker des données, useEffect demande que userService.getAllUsers() récupère EN DIFFERÉ les données de la base de données et map permet de parcourir ces données pour les afficher selon les props.
+import React, { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { userService } from "../../../src/services/user.service";
@@ -8,40 +8,63 @@ const User = () => {
 
     let navigate = useNavigate()
 
-    // Ce hook
+    // Ce hook prépare une place pour stocker les données de la bdd une fois qu'elles seront récupérées.
     const [users, setUsers] = useState([])
 
-    // Ce hook s'active en même temps qu'un événement particulier choisis par le dev et si il y a pas d'événement particulier on met un tableau vide pour q'il s'execute qu'une seule fois:
-    useEffect(() => {
-        userService.getAllUsers()
-        .then(res => {
-            console.log(res.data)
-            setUsers(res.data)
-    })
-        .catch(err => console.log(err))
-    }, [])
+    // Ce hook sert de pense bête pour que mon code se rappel d'une fonction décrite plus bas.
+    const flag = useRef(false)
 
-    // const userSelectId = (userId) => {
-    //     console.log('click')
-    //     navigate("../useredit/"+userId)
-    // }
+    // Ce hook s'active en même temps qu'un événement particulier choisis par le dev et si il y a pas d'événement particulier on met un tableau vide pour qu'il s'execute qu'une seule fois.
+    // useEffect permet de charger progressivement les donées car il demande a charger les données de la bdd apres que la page soit chargée.
+    // On ajoute une fonction qui fait une requête HTTP pour récupérer les données des utilisateurs depuis la bdd.
+    useEffect(() => {
+
+        // On utilise flag.current pour éviter de rappeler userService.getAllUsers() plus d'une fois lors du rendu du composant:
+        if (flag.current === false) {
+            userService.getAllUsers()
+                .then(res => {
+                    console.log(res.data)
+                    setUsers(res.data)
+                })
+                .catch(err => console.log(err))
+        }
+        return () => flag.current = true
+    }, [])
 
     return (
         <div className="user">
             <h1>User</h1>
-            {/* <button onClick={() => userSelectId(2)}>User 2</button> */}
             <table>
                 <thead>
                     <tr>
-                        <th>id</th>
+                        <th>#</th>
                         <th>Prénom</th>
                         <th>Nom</th>
-                        <th>Adresse</th>
-                        <th>Téléphone</th>
                         <th>Email</th>
-                        <th>Hash</th>
+                        <th>Téléphone</th>
+                        <th>Adresse</th>
+                        <th>Ville</th>
+                        <th>Code Postale</th>
                     </tr>
                 </thead>
+                <tbody>
+                    {
+                        // On utilise map pour parcourir le tableau des données des users et on utilise les props pour l'organiser.
+                        users.map(user => (
+                            // Key explique a réact qu'il sagit de la donnée importante et unique qui identifie les users:
+                            <tr key={user.id}> {}
+                                <td>{user.id}</td>
+                                <td>{user.firstname}</td>
+                                <td>{user.lastname}</td>
+                                <td>{user.email}</td>
+                                <td>{user.phone}</td>
+                                <td>{user.address}</td>
+                                <td>{user.city}</td>
+                                <td>{user.postalcode}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
             </table>
         </div>
     );
